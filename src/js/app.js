6c60d1f4b0ea99411c1d45fa8f8c51a69867e4fe
Ahmed Ali <a.husseinzaki@gmail.com>
2017-12-24 16:08:00 -0800

    Change map style

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ function initMap() {
   // Create a map object and specify the DOM element for display.
   const map = new google.maps.Map(document.getElementById('map'), {
     center: new google.maps.LatLng(41.850033, -87.6500523),
-    zoom: 5
+    zoom: 5,
+    styles: style,
   });
 }
